f03acf788f948e4598a1a6f6a5a16e3add4a5528
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2023-07-23 23:59:37 +0200

    implemented logicDeletion and Search in Btree [Note] Still works with just ints

--- Btree.go
+++ Btree.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 const q int = 2
 
 // konstanta t je neki stepen ovog drveta koji ja mislim da se odredjuje iz nekog log fajla pri pokretanju
@@ -15,161 +11,162 @@ type Node struct { //parametri i deklaracija strukture cvora
 	children []*Node //lista pokazivaca na Nodeove koji su children
 }
 
+type Data struct { //TODO: kasnije prilagoditi kod ovome, tek kada resim ono sa insertom i proverim da sve dobro radi za int neka se sadrzi Data
+	key    int
+	value  []byte
+	online bool
+}
+
 type Tree struct {
 	root *Node
 }
 
+// ============================OVAJ DEO CE VEROVATNO BITI TOTALNO NEKORISCEN I NEPOTREBAN==================================
 // helpers from stack overflow(neke sam i ja kucao)
-func (node *Node) Contains(value int) bool {
-	for _, item := range node.keys {
-		if item == value {
-			return true
-		}
-	}
-	return false
-}
-func (node *Node) RemoveKeyFromNode(keyToRemove int) {
-	var result []int
+//func (node *Node) RemoveKeyFromNode(keyToRemove int) {
+//	var result []int
+//
+//	for _, v := range node.keys {
+//		if v != keyToRemove {
+//			result = append(result, v)
+//		}
+//	}
+//
+//	node.keys = result
+//}
+//func (node *Node) RemoveChildByIndex(index int) {
+//	if index < 0 || index >= len(node.children) {
+//		return // Index out of range, do nothing
+//	}
+//
+//	node.children = append(node.children[:index], node.children[index+1:]...)
+//}
+//func (node *Node) GetChildToContinueDeletion(key int) int {
+//	for ind, val := range node.keys {
+//		if val > key {
+//			return ind
+//		}
+//	}
+//	return len(node.keys)
+//}
+//func (node *Node) findKeyIndex(key int) int {
+//	for i, v := range node.keys {
+//		if v == node.keys[i] {
+//			return i
+//		}
+//	}
+//	return -1 // Value not found in the list
+//}
+//
+//// deletion functions
+//func (t *Tree) Delete(key int) {
+//	if t.root == nil {
+//		fmt.Print("Prazno stablo")
+//		return
+//	}
+//
+//	var end bool = false
+//	x := t.root
+//	for end != true {
+//		if x.Contains(key) {
+//			if x.leaf {
+//				x.DeleteFromLeaf(key)
+//				end = true
+//			} else {
+//				x.DeleteFromNonLeaf(key)
+//				end = true
+//			}
+//		} else {
+//			if x.leaf {
+//				fmt.Print("Nema kljuca u stablu")
+//				return
+//			}
+//
+//			childIndex := x.GetChildToContinueDeletion(key) //treba ti index koje dete ces da proveris da li ima dovoljno kljuceva
+//			if len(x.children[childIndex].keys) < q {
+//				x.FillNode(childIndex)
+//			}
+//			x = x.children[childIndex]
+//		}
+//	}
+//
+//	//y = nil // mesto gde se cuva roditelj od x
+//	//
+//	//for x.Contains(key) != true {
+//	//	if x.leaf == true {
+//	//		//TODO nema kljuca
+//	//	} else {
+//	//		//TODO nadji dobrog childa -> proveri da li ima t kljuceva
+//	//		/* if nema min t kljuceva -> odredi nacin da spustis kljuc iz x dole(Fill) -> spusti se dole i zovi brisanje
+//	//		 */
+//	//	}
+//	//}
+//	//if x.leaf == true {
+//	//	if len(x.keys) >= q {
+//	//		x.DeleteFromLeaf(key)
+//	//	} else {
+//	//		x.DeleteFromLeaf(key)
+//	//
+//	//	}
+//	//} else {
+//	//	//TODO obrisi, a da nije leaf, drugacija fja
+//	//}
+//}
+//
+//func (node *Node) DeleteFromLeaf(key int) {
+//	node.RemoveKeyFromNode(key)
+//}
+//
+//func (node *Node) FillNode(index int) {
+//	if len(node.children[index-1].keys) >= q { //TODO a sta cemo sa decom ovde jel to predstavlja problem?
+//		node.insertNonFull(node.children[index-1].keys[len(node.children[index-1].keys)-1], nil)                  //uzmi poslednji(najveci) kljuc levog deteta i sibni gore
+//		node.children[index-1].RemoveKeyFromNode(node.children[index-1].keys[len(node.children[index-1].keys)-1]) //izbaci poslednji iz levog deteta
+//		node.children[index].insertNonFull(node.keys[index], nil)
+//		node.RemoveKeyFromNode(node.keys[index])
+//	} else if len(node.children[index+1].keys) >= q {
+//		node.insertNonFull(node.children[index+1].keys[0], nil) //isto ko gore samo za desni
+//		node.children[index+1].RemoveKeyFromNode(node.children[index+1].keys[0])
+//		node.children[index].insertNonFull(node.keys[index], nil)
+//		node.RemoveKeyFromNode(node.keys[index])
+//	} else {
+//		vrednostiKljucevaKojeSeBrisuIzParenta := []int{}
+//		for i := len(node.children[index-1].keys) - 1; i != -1; i-- {
+//			if i <= index {
+//				node.children[index].insertNonFull(node.keys[i], nil)
+//				vrednostiKljucevaKojeSeBrisuIzParenta = append(vrednostiKljucevaKojeSeBrisuIzParenta, node.keys[i])
+//			}
+//			node.children[index].insertNonFull(node.children[index-1].keys[i], nil)
+//			node.insertNodeArray(0, node.children[index-1].children[i])
+//		}
+//		node.RemoveChildByIndex(index - 1)
+//		for _, vrKljucaZaBrisanjeIzParenta := range vrednostiKljucevaKojeSeBrisuIzParenta {
+//			node.RemoveKeyFromNode(vrKljucaZaBrisanjeIzParenta)
+//		}
+//	}
+//}
+//
+//func (node *Node) DeleteFromNonLeaf(key int) {
+//	indexOfKeyToDelete := node.findKeyIndex(key)
+//	if len(node.children[indexOfKeyToDelete].keys) >= q { //leva strana
+//		node.keys[indexOfKeyToDelete] = node.children[indexOfKeyToDelete].keys[len(node.children[indexOfKeyToDelete].keys)]
+//		node.children[indexOfKeyToDelete].RemoveKeyFromNode(node.keys[indexOfKeyToDelete])
+//	} else if len(node.children[indexOfKeyToDelete+1].keys) >= q {
+//		node.keys[indexOfKeyToDelete] = node.children[indexOfKeyToDelete+1].keys[0]
+//		node.children[indexOfKeyToDelete+1].RemoveKeyFromNode(node.keys[indexOfKeyToDelete])
+//	} else {
+//		for i := len(node.children[indexOfKeyToDelete-1].keys) - 1; i != -1; i-- {
+//			node.children[indexOfKeyToDelete].insertNonFull(node.children[indexOfKeyToDelete-1].keys[i], nil)
+//			node.insertNodeArray(0, node.children[indexOfKeyToDelete-1].children[i])
+//		}
+//		node.RemoveKeyFromNode(key)
+//		node.RemoveChildByIndex(indexOfKeyToDelete - 1)
+//		//TODO After merging if the parent node has less than the minimum number of keys then, look for the siblings as in Case I.
+//		//videti sutra jos ovo al nmg nista mi ovo ne vredi (sem da predjem u glavi ja to) dok ne resim insert...
+//	}
+//	//TODO ostao mi je i Case III koji nisam ni pogledao jer ga nema u pseudo kodu na ChatGPT, zato nez dal to treba da radim ovde uopste ili je vec pokriveno jer je ovo sve kombinacija sajta i ChatGPT
+//}
 
-	for _, v := range node.keys {
-		if v != keyToRemove {
-			result = append(result, v)
-		}
-	}
-
-	node.keys = result
-}
-func (node *Node) RemoveChildByIndex(index int) {
-	if index < 0 || index >= len(node.children) {
-		return // Index out of range, do nothing
-	}
-
-	node.children = append(node.children[:index], node.children[index+1:]...)
-}
-func (node *Node) GetChildToContinueDeletion(key int) int {
-	for ind, val := range node.keys {
-		if val > key {
-			return ind
-		}
-	}
-	return len(node.keys)
-}
-func (node *Node) findKeyIndex(key int) int {
-	for i, v := range node.keys {
-		if v == node.keys[i] {
-			return i
-		}
-	}
-	return -1 // Value not found in the list
-}
-
-// deletion functions
-func (t *Tree) Delete(key int) {
-	if t.root == nil {
-		fmt.Print("Prazno stablo")
-		return
-	}
-
-	var end bool = false
-	x := t.root
-	for end != true {
-		if x.Contains(key) {
-			if x.leaf {
-				x.DeleteFromLeaf(key)
-				end = true
-			} else {
-				x.DeleteFromNonLeaf(key)
-				end = true
-			}
-		} else {
-			if x.leaf {
-				fmt.Print("Nema kljuca u stablu")
-				return
-			}
-
-			childIndex := x.GetChildToContinueDeletion(key) //treba ti index koje dete ces da proveris da li ima dovoljno kljuceva
-			if len(x.children[childIndex].keys) < q {
-				x.FillNode(childIndex)
-			}
-			x = x.children[childIndex]
-		}
-	}
-
-	//y = nil // mesto gde se cuva roditelj od x
-	//
-	//for x.Contains(key) != true {
-	//	if x.leaf == true {
-	//		//TODO nema kljuca
-	//	} else {
-	//		//TODO nadji dobrog childa -> proveri da li ima t kljuceva
-	//		/* if nema min t kljuceva -> odredi nacin da spustis kljuc iz x dole(Fill) -> spusti se dole i zovi brisanje
-	//		 */
-	//	}
-	//}
-	//if x.leaf == true {
-	//	if len(x.keys) >= q {
-	//		x.DeleteFromLeaf(key)
-	//	} else {
-	//		x.DeleteFromLeaf(key)
-	//
-	//	}
-	//} else {
-	//	//TODO obrisi, a da nije leaf, drugacija fja
-	//}
-}
-
-func (node *Node) DeleteFromLeaf(key int) {
-	node.RemoveKeyFromNode(key)
-}
-
-func (node *Node) FillNode(index int) {
-	if len(node.children[index-1].keys) >= q { //TODO a sta cemo sa decom ovde jel to predstavlja problem?
-		node.insertNonFull(node.children[index-1].keys[len(node.children[index-1].keys)-1], nil)                  //uzmi poslednji(najveci) kljuc levog deteta i sibni gore
-		node.children[index-1].RemoveKeyFromNode(node.children[index-1].keys[len(node.children[index-1].keys)-1]) //izbaci poslednji iz levog deteta
-		node.children[index].insertNonFull(node.keys[index], nil)
-		node.RemoveKeyFromNode(node.keys[index])
-	} else if len(node.children[index+1].keys) >= q {
-		node.insertNonFull(node.children[index+1].keys[0], nil) //isto ko gore samo za desni
-		node.children[index+1].RemoveKeyFromNode(node.children[index+1].keys[0])
-		node.children[index].insertNonFull(node.keys[index], nil)
-		node.RemoveKeyFromNode(node.keys[index])
-	} else {
-		vrednostiKljucevaKojeSeBrisuIzParenta := []int{}
-		for i := len(node.children[index-1].keys) - 1; i != -1; i-- {
-			if i <= index {
-				node.children[index].insertNonFull(node.keys[i], nil)
-				vrednostiKljucevaKojeSeBrisuIzParenta = append(vrednostiKljucevaKojeSeBrisuIzParenta, node.keys[i])
-			}
-			node.children[index].insertNonFull(node.children[index-1].keys[i], nil)
-			insertNodeArray(node.children, 0, node.children[index-1].children[i])
-		}
-		node.RemoveChildByIndex(index - 1)
-		for _, vrKljucaZaBrisanjeIzParenta := range vrednostiKljucevaKojeSeBrisuIzParenta {
-			node.RemoveKeyFromNode(vrKljucaZaBrisanjeIzParenta)
-		}
-	}
-}
-
-func (node *Node) DeleteFromNonLeaf(key int) {
-	indexOfKeyToDelete := node.findKeyIndex(key)
-	if len(node.children[indexOfKeyToDelete].keys) >= q { //leva strana
-		node.keys[indexOfKeyToDelete] = node.children[indexOfKeyToDelete].keys[len(node.children[indexOfKeyToDelete].keys)]
-		node.children[indexOfKeyToDelete].RemoveKeyFromNode(node.keys[indexOfKeyToDelete])
-	} else if len(node.children[indexOfKeyToDelete+1].keys) >= q {
-		node.keys[indexOfKeyToDelete] = node.children[indexOfKeyToDelete+1].keys[0]
-		node.children[indexOfKeyToDelete+1].RemoveKeyFromNode(node.keys[indexOfKeyToDelete])
-	} else {
-		for i := len(node.children[indexOfKeyToDelete-1].keys) - 1; i != -1; i-- {
-			node.children[indexOfKeyToDelete].insertNonFull(node.children[indexOfKeyToDelete-1].keys[i], nil)
-			insertNodeArray(node.children, 0, node.children[indexOfKeyToDelete-1].children[i])
-		}
-		node.RemoveKeyFromNode(key)
-		node.RemoveChildByIndex(indexOfKeyToDelete - 1)
-		//TODO After merging if the parent node has less than the minimum number of keys then, look for the siblings as in Case I.
-		//videti sutra jos ovo al nmg nista mi ovo ne vredi (sem da predjem u glavi ja to) dok ne resim insert...
-	}
-	//TODO ostao mi je i Case III koji nisam ni pogledao jer ga nema u pseudo kodu na ChatGPT, zato nez dal to treba da radim ovde uopste ili je vec pokriveno jer je ovo sve kombinacija sajta i ChatGPT
-}
+// =========================================================================================================================
 
 // helper functions from stack overflow
 // insertuje na odredjeni index u arrayu, a ove ostale pomeri za jedno mesto desno 										-> =====> mogucnost optimizacije je slanje argumenata kao pokazivace...menjace i poziv same fje al to mzd na kraju
@@ -190,26 +187,31 @@ func (node *Node) insertIntArray(index int, value int, t *Tree) {
 		node.keys[index] = value
 	}
 }
-
-// TODO: kad ustanovis zasto ne valja insertIntArray moras promeniti i insertNodeArray
-func insertNodeArray(a []*Node, index int, value *Node) []*Node { //insertuje na odredjeni index u arrayu samo za Node-ove
-	if len(a) == index { // nil or empty slice or after last element
-		return append(a, value)
+func (node *Node) insertNodeArray(index int, value *Node) { //insertuje na odredjeni index u arrayu samo za Node-ove
+	if len(node.children) == index { // nil or empty slice or after last element
+		node.children = append(node.children, value)
 	}
-	a = append(a[:index+1], a[index:]...) // index < len(a)
-	a[index] = value
-	return a
+	node.children = append(node.children[:index+1], node.children[index:]...) // index < len(a)
+	node.children[index] = value
 }
-func getAppropriateChildIndex(keys []int, key int) int { //vratice index deteta koje treba da proveravamo
-	for i, num := range keys {
+func (node *Node) getAppropriateChildIndex(key int) int { //vratice index deteta koje treba da proveravamo
+	for i, num := range node.keys {
 		if num > key {
 			return i
 		}
-		if i+1 == len(keys) {
+		if i+1 == len(node.keys) {
 			return i + 1
 		}
 	}
 	return -1 // ovo se nece nikada desiti ali se baca error ako ne napisem
+}
+func (node *Node) Contains(value int) int {
+	for ind, item := range node.keys {
+		if item == value {
+			return ind
+		}
+	}
+	return -1
 }
 
 // insertion functions
@@ -240,8 +242,8 @@ func (node *Node) splitCurrent(root bool, parent *Node) *Node {
 		childRight.keys = node.keys[(len(node.keys)/2)+1:]
 
 		indexForChildren := parent.insertNonFull(keyForParent, nil) //deca idu na index parenta i index parenta plus 1
-		insertNodeArray(parent.children, indexForChildren, childLeft)
-		insertNodeArray(parent.children, indexForChildren+1, childRight)
+		parent.insertNodeArray(indexForChildren, childLeft)
+		parent.insertNodeArray(indexForChildren+1, childRight)
 		return nil
 	}
 }
@@ -259,17 +261,49 @@ func (t *Tree) Insert(key int) {
 	x := t.root //privremeni node da ne bih pomerao koren kao
 
 	for x.leaf == false { //isto sto i while hahahah
-		index := getAppropriateChildIndex(x.keys, key)
+		index := x.getAppropriateChildIndex(key)
 		y := x.children[index]
 		if (len(y.keys)) == 2*q-1 {
 			y.splitCurrent(false, x)
-			indexOfNextChild := getAppropriateChildIndex(x.keys, key)
+			indexOfNextChild := x.getAppropriateChildIndex(key)
 			x = x.children[indexOfNextChild]
 		} else {
 			x = y
 		}
 	}
 	x.insertNonFull(key, t)
+}
+
+// search functions
+func (t *Tree) Search(key int) (int, *Node) { //retVal je index u Node(keys) na kom se nalazi trazena vrednost, -1 ako nema vrednosti logicno
+	if t.root == nil {
+		return -1, nil
+	}
+
+	x := t.root
+	for x.leaf != true {
+		indexOfSearchedKey := x.Contains(key)
+		if indexOfSearchedKey != -1 {
+			return indexOfSearchedKey, x
+		} else {
+			indexOfChildToContinue := x.getAppropriateChildIndex(key)
+			x = x.children[indexOfChildToContinue]
+		}
+	}
+
+	indexOfSearchedKey := x.Contains(key)
+	if indexOfSearchedKey != -1 {
+		return indexOfSearchedKey, x
+	}
+	return -1, nil
+}
+
+func (t *Tree) Deletion(key int) {
+	_, node := t.Search(key)
+	if node != nil {
+		//node.keys[_].online = false
+		//ovako nekako ce to izgledati, udji u kljuceve noda, uzmi tacno koji si dobio pretragom i njegov status stavi na nekativan
+	}
 }
 
 func main() {
